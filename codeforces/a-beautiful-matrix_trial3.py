3920cd61963793b2aae03af2176e3e5393630a44
Getayawkal Tamrat <gtamrat33@gmail.com>
2026-02-23 23:54:14 +0300

    Add solution for A - Beautiful Matrix (trial 3)

---
+++ codeforces/a-beautiful-matrix_trial3.py
@@ -0,0 +1,14 @@
+matrix = []
+
+for _ in range(5):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            row_pos = i + 1
+            col_pos = j + 1
+
+moves = abs(row_pos - 3) + abs(col_pos - 3)
+print(moves)
